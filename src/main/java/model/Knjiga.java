29b211bf2a921d5fd0c3b2af6ac31a60f57134fa
Nenad <norlovic@gmail.com>
2017-01-16 19:31:56 +0100

    Uradjena klasa Knjiga fixes #3

---
+++ src/main/java/model/Knjiga.java
@@ -0,0 +1,95 @@
+package model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+/**
+ * Created by androiddevelopment on 16.1.17..
+ */
+
+@DatabaseTable(tableName = "knjiga")
+public class Knjiga {
+    public static final String POLJE_NASLOV="naslov";
+    public static final String POLJE_BROJ_STRANA="broj_strana";
+    public static final String POLJE_DATUM_IZDAVANJA="datum_izdavanja";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_NASLOV,canBeNull = false, unique=false)
+    private String naslov;
+    @DatabaseField(columnName = POLJE_BROJ_STRANA,canBeNull = false, unique=false)
+    private int brojStrana;
+    @DatabaseField(columnName = POLJE_DATUM_IZDAVANJA,canBeNull = false, unique=false)
+    private Date datumIzdanja;
+    private boolean prisutna;
+
+    @ForeignCollectionField(foreignFieldName = "oblast")
+    private ForeignCollection<Oblast> oblasti;
+
+    public ForeignCollection<Oblast> getOblasti() {
+        return oblasti;
+    }
+
+    public void setOblasti(ForeignCollection<Oblast> oblasti) {
+        this.oblasti = oblasti;
+    }
+
+    public Knjiga() {
+
+    }
+
+    public Knjiga(String naslov, int brojStrana, Date datumIzdanja) {
+        this.naslov = naslov;
+        this.brojStrana = brojStrana;
+        this.datumIzdanja = datumIzdanja;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public int getBrojStrana() {
+        return brojStrana;
+    }
+
+    public void setBrojStrana(int brojStrana) {
+        this.brojStrana = brojStrana;
+    }
+
+    public Date getDatumIzdanja() {
+        return datumIzdanja;
+    }
+
+    public void setDatumIzdanja(Date datumIzdanja) {
+        this.datumIzdanja = datumIzdanja;
+    }
+
+    public boolean isPrisutna() {
+        return prisutna;
+    }
+
+    public void setPrisutna(boolean prisutna) {
+        this.prisutna = prisutna;
+    }
+
+    @Override
+    public String toString() {
+        return "Knjiga{id = " + id + " naslov = " + naslov + " brojStrana = " + brojStrana + "datumIzdavanja = " +
+                datumIzdanja +'}';
+    }
+}
